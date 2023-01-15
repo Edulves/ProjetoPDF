b48f5f9af4ffc74959338a7b87cda17a15f1567e
Edulves <Eduardo.lves@outlook.com>
2023-01-15 20:28:28 -0300

    Add project files.

---
+++ ProjetoPDF/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ProjetoPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRenomear = new System.Windows.Forms.Button();
+            this.btnSeparar = new System.Windows.Forms.Button();
+            this.btnJuntar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clique para selecionar o arquivo de origem";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Origem";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Destino";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(133, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(393, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Clique para selecionar a pasta de destino";
+            // 
+            // btnRenomear
+            // 
+            this.btnRenomear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRenomear.Location = new System.Drawing.Point(43, 96);
+            this.btnRenomear.Name = "btnRenomear";
+            this.btnRenomear.Size = new System.Drawing.Size(147, 51);
+            this.btnRenomear.TabIndex = 6;
+            this.btnRenomear.Text = "Renomear";
+            this.btnRenomear.UseVisualStyleBackColor = true;
+            // 
+            // btnSeparar
+            // 
+            this.btnSeparar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeparar.Location = new System.Drawing.Point(211, 96);
+            this.btnSeparar.Name = "btnSeparar";
+            this.btnSeparar.Size = new System.Drawing.Size(147, 51);
+            this.btnSeparar.TabIndex = 7;
+            this.btnSeparar.Text = "Separar";
+            this.btnSeparar.UseVisualStyleBackColor = true;
+            // 
+            // btnJuntar
+            // 
+            this.btnJuntar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJuntar.Location = new System.Drawing.Point(379, 96);
+            this.btnJuntar.Name = "btnJuntar";
+            this.btnJuntar.Size = new System.Drawing.Size(147, 51);
+            this.btnJuntar.TabIndex = 8;
+            this.btnJuntar.Text = "Juntar";
+            this.btnJuntar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 158);
+            this.Controls.Add(this.btnJuntar);
+            this.Controls.Add(this.btnSeparar);
+            this.Controls.Add(this.btnRenomear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRenomear;
+        private System.Windows.Forms.Button btnSeparar;
+        private System.Windows.Forms.Button btnJuntar;
+    }
+}
+
---
+++ ProjetoPDF/PDF.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPDF
+{
+    class PDF
+    {
+        private List<string> Pedidos;
+        private string pedido;
+
+        public void Renomear()
+        {
+
+        }
+
+        public void Separar()
+        {
+            // teste
+        }
+
+        public void Juntar()
+        {
+
+        }
+    }
+}
